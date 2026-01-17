ad7da7894d7aa70a69399ed43ab8040cf1085ea6
Little Boy's h1eudayne <voduchieu42@gmail.com>
2026-01-17 13:10:46 +0700

    Components Part 1

---
+++ React_Component_1/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Footer from "./Footer";
+import Navigation from "./Navigation";
+import Pets from "./Pets";
+
+function App() {
+  return (
+    <>
+      <Navigation />
+      <Pets />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
